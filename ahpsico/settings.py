fb149b427a9da54d83fdb373055f6bbd8f7990d2
João Pegoraro <hahmraro@tuta.io>
2023-07-08 07:00:36 -0300

    Adicionei template do .env

--- ahpsico/settings.py
+++ ahpsico/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = str(os.environ.get("DEBUG")) == "1"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
 
 
 # Application definition
